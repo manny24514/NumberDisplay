3762704f7ad487a6f4e1fd4f76e7d80b3564740b
MANUEL HERNANDEZ <rl106926@rlas-macair-36.local>
2018-09-25 11:38:39 -0500

    Initial Commit

---
+++ NumberDisplay/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  NumberDisplay
+//
+//  Created by MANUEL HERNANDEZ on 9/24/18.
+//  Copyright © 2018 MANUEL HERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func onNumberButtonTapped(_ sender: Any) {
+        let numberToDisplay = numberTextField.text
+        numberLabel.text = numberToDisplay
+        
+        
+    
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
